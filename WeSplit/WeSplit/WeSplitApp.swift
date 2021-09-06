57249aae17d02273291447642d760141f0c24eb0
Dielson Sales <dielsonscarvalho@gmail.com>
2021-09-06 11:36:39 -0300

    Create project WeSplit

---
+++ WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Dielson Sales on 05/09/21.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
