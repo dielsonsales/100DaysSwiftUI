7c190f2a5c4ef23786b5c12c70ad9a2075719ae6
Dielson Sales <dielsonscarvalho@gmail.com>
2021-09-07 16:47:18 -0300

    Experiment with the first SwiftUI components

--- WeSplit/WeSplit/ContentView.swift
+++ WeSplit/WeSplit/ContentView.swift
@@ -8,9 +8,28 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var tapCount = 0
+    let items = ["One", "Two", "Three"]
+    @State private var selectedItem = 0
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            VStack {
+                Button("Tap count: \(tapCount)") {
+                    self.tapCount += 1
+                }
+                Form {
+                    ForEach(0 ..< self.items.count) { index in
+                        Text("\(self.items[index])")
+                    }
+                }
+                .navigationBarTitle("WeSplit")
+                Picker("Select something", selection: $selectedItem) {
+                    ForEach(0 ..< self.items.count) { index in
+                        Text(self.items[index])
+                    }
+                }
+            }
+        }
     }
 }
 
